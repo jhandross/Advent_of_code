2fcfa3ddd1d506c4c51d3700a5b2b23a10f67493
jhandross <141276453+jhandross@users.noreply.github.com>
2024-02-03 12:15:36 +0100

    Add files via upload

--- 2017/01.py
+++ 2017/01.py
@@ -1,1 +1,9 @@
-
+data = open('01.txt').read().strip()
+data = list(map(int, data))
+ 
+def solve(digits, second_part=False):
+    jump = 1 if not second_part else len(digits)//2
+    return sum(n for i, n in enumerate(digits) if n == digits[i-jump])
+ 
+print('Part 1:',solve(data))
+print('Part 2:',solve(data, 'part 2'))
