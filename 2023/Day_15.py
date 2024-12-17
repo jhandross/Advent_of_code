cfcf9078a2c5bc2d74bbbf66212623d761e2f741
jhandross <141276453+jhandross@users.noreply.github.com>
2024-12-17 15:02:18 +0100

    Add files via upload

--- 2023/Day_15.py
+++ 2023/Day_15.py
@@ -1,51 +1,19 @@
-import re
 from collections import defaultdict
+from functools import reduce
 
-with open('15.txt', 'r') as f:
-    puzzle_input = f.read()
+hash = lambda label: reduce(lambda cur, ch: (cur + ord(ch)) * 17 % 256, label, 0)
 
+with open('15.txt') as f:
+    labels = f.read().split(',')
 
-def part1(puzzle_input):
-    total = 0
-    for step in puzzle_input.split(','):
-        current_val = 0
-        for char in step:
-            current_val += ord(char)
-            current_val *= 17
-            current_val %= 256
-        total += current_val
+def part2():
+    boxes = defaultdict(dict)
+    for label in labels:
+        if '-' in label:
+            boxes[hash(label[:-1])].pop(label[:-1], None)
+        elif '=' in label:
+            lens, focal = label.split('=')
+            boxes[hash(lens)][lens] = int(focal)
+    return sum((1+box) * (i+1) * focal for box in boxes for i, focal in enumerate(boxes[box].values()))
 
-    return total
-
-
-def part2(puzzle_input):
-
-    labels = defaultdict(list)
-    lenses = defaultdict(list)
-    regex = r'(\w+)(=|-)(\d+)?'
-    for label, op, focal_len in re.findall(regex, puzzle_input):
-        hash = 0
-        for char in label:
-            hash = (hash + ord(char)) * 17 % 256
-
-        if label in labels[hash]:
-            i = labels[hash].index(label)
-            if op == '-':
-                labels[hash].pop(i)
-                lenses[hash].pop(i)
-            else:
-                lenses[hash][i] = int(focal_len)
-        elif op == '=':
-            labels[hash].append(label)
-            lenses[hash].append(int(focal_len))
-
-    total = 0
-    for box, lenses in lenses.items():
-        for i, focal_len in enumerate(lenses, start=1):
-            total += (box+1) * i * focal_len
-        
-    return total
-
-
-print('Part 1:', part1(puzzle_input))
-print('Part 2:', part2(puzzle_input))
+print(sum(hash(label) for label in labels), part2())
