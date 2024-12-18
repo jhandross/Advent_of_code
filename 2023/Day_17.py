756a8c88fb09509c4ee5dc68b0bb1b534b99f237
jhandross <141276453+jhandross@users.noreply.github.com>
2024-12-18 01:07:20 +0100

    Add files via upload

--- 2023/Day_17.py
+++ 2023/Day_17.py
@@ -1,80 +1,18 @@
-from heapq import heappop, heappush
+import heapq
 
-with open('17.txt', 'r') as f:
-    puzzle_input = f.read()
+grid = {(x, y): int(n) for y, r in enumerate(open("17.txt")) for x, n in enumerate(r.strip())}
 
-
-def part1(puzzle_input):
-    grid = [[int(d) for d in line] for line in puzzle_input.split('\n')]
-    m, n = len(grid), len(grid[0])
-    directions = [(-1, 0), (0, -1), (0, 1), (1, 0)]
-
-    # tuple: (heat-loss, x-coord, y-coord, length-of-current-run, x-direction, y-direction)
-    q = [(0, 0, 0, 0, 0, 0)] 
-    visited = set()
+def sol(mn, mx):
+    q, v, e = [(grid[1, 0], 1, 0, 1, 0, 0), (grid[0, 1], 0, 1, 0, 1, 0)], set(), max(grid)
     while q:
-        loss, x, y, k, dx, dy = heappop(q)
+        h, x, y, dx, dy, s = heapq.heappop(q)
+        if (x, y) == e and mn <= s: return h
+        if (x, y, dx, dy, s) in v: continue
+        v.add((x, y, dx, dy, s))
+        if s < mx-1 and (p:=(x+dx, y+dy)) in grid: heapq.heappush(q, (h+grid[p], *p, dx, dy, s+1))
+        if mn <= s:
+            for xx, yy in [(dy, -dx), (-dy, dx)]:
+                if (p:=(x+xx, y+yy)) in grid: heapq.heappush(q, (h+grid[p], *p, xx, yy, 0))
 
-        if x == m-1 and y == n-1:
-            break
-
-        if any((x, y, k_, dx, dy) in visited for k_ in range(1, k+1)):
-            continue
-    
-        visited.add((x, y, k, dx, dy))
-        for new_dx, new_dy in directions:
-            straight = (new_dx == dx and new_dy == dy)
-            new_x, new_y = x + new_dx, y + new_dy
-
-            if any((new_dx == -dx and new_dy == -dy,
-                    k == 3 and straight,
-                    new_x < 0, new_y < 0, 
-                    new_x == m, new_y == n)):
-                continue
-
-            new_k = k + 1 if straight else 1            
-            heappush(q, (loss + grid[new_x][new_y], new_x, new_y, new_k, new_dx, new_dy))
-
-    return loss
-
-
-def part2(puzzle_input):
-    grid = [[int(d) for d in line] for line in puzzle_input.split('\n')]
-    m, n = len(grid), len(grid[0])
-    directions = [(-1, 0), (0, -1), (0, 1), (1, 0)]
-
-    # tuple: (heat-loss, x-coord, y-coord, length-of-current-run, x-direction, y-direction)
-    q = [(0, 0, 0, 0, 0, 1), (0, 0, 0, 0, 1, 0)] 
-    visited = set()
-    while q:
-        loss, x, y, k, dx, dy = heappop(q)
-
-        if x == m-1 and y == n-1:
-            if k < 4:
-                continue
-            break
-
-        if (x, y, k, dx, dy) in visited:
-            continue
-    
-        visited.add((x, y, k, dx, dy))
-
-        for new_dx, new_dy in directions:
-            straight = (new_dx == dx and new_dy == dy)
-            new_x, new_y = x + new_dx, y + new_dy
-
-            if any((new_dx == -dx and new_dy == -dy,
-                    k == 10 and straight,
-                    k < 4 and not straight,
-                    new_x < 0, new_y < 0, 
-                    new_x == m, new_y == n)):
-                continue
-
-            new_k = k + 1 if straight else 1
-            heappush(q, (loss + grid[new_x][new_y], new_x, new_y, new_k, new_dx, new_dy))
-
-    return loss
-
-
-print('Part 1:', part1(puzzle_input))
-print('Part 2:', part2(puzzle_input))
+print(sol(0, 3))
+print(sol(3, 10))
